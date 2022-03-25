15e4ab032040d614c5638b2db23d26f33b924f88
Joel Sprouse <joel.sprouse11@gmail.com>
2022-03-25 09:23:36 +0000

    Attempted this thing

--- js/playerMovement.js
+++ js/playerMovement.js
@@ -7,27 +7,40 @@ var upPress, rightPressed, downPressed, leftPressed = false;
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
+function tryPlayerRight() {
+	if (document.getElementById("player").offsetLeft < document.getElementById("FloorFive").width - document.getElementById("player").width) {
+		console.log("can go right");
+	}
+	if (rightPressed) {
+		document.getElementById("player").style.left = document.getElementById("player").offsetLeft + 10 + "px";
+		tryPlayerRight();
+	}
+}
+
 function keyDownHandler(e) {
-    if (e.code == "ArrowRight") {
-        rightPressed = true;
-    } else if (e.code == 'ArrowLeft') {
-        leftPressed = true;
-    } else if (e.code == 'ArrowUp') {
-        upPressed = true;
-    } else if (e.code == 'ArrowDown') {
-        downPressed = true;
-    }
+	if (e.code == "ArrowRight") {
+		rightPressed = true;
+	}
+	//while (e.code == 'ArrowLeft') {
+	//leftPressed = true;
+	//}
+	//while (e.code == 'ArrowUp') {
+	//upPressed = true;
+	//}
+	//while (e.code == 'ArrowDown') {
+	//downPressed = true;
+	//}
 }
 function keyUpHandler(e) {
-    if (e.code == "ArrowRight") {
-        rightPressed = false;
-    }
-    else if (e.code == 'ArrowLeft') {
-        leftPressed = false;
-    } else if (e.code == 'ArrowUp') {
-        upPressed = false;
-    } else if (e.code == 'ArrowDown') {
-        downPressed = false;
-    }
+	if (e.code == "ArrowRight") {
+		rightPressed = false;
+	}
+	else if (e.code == 'ArrowLeft') {
+		leftPressed = false;
+	} else if (e.code == 'ArrowUp') {
+		upPressed = false;
+	} else if (e.code == 'ArrowDown') {
+		downPressed = false;
+	}
 }
 
