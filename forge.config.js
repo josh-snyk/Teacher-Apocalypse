d5ddc5753a4c05c344c0474750dbea983354a8c3
Joel Sprouse <joel.sprouse11@gmail.com>
2022-03-31 00:27:26 +0100

    Added Electron Forge

--- forge.config.js
+++ forge.config.js
@@ -1,22 +1,50 @@
 // JavaScript source code
 module.exports = {
-    makers: [
-        {
-            name: '@electron-forge/maker-zip',
-            platforms: ['darwin', 'linux'],
-            config: {
-                // Config here
-            }
-        },
-        {
-            name: '@electron-forge/publisher-github',
-            config: {
-                repository: {
-                    owner: '14sprouj',
-                    name: 'Teacher-Apocalypse'
-                },
-                prerelease: true
-            }
-        }
-    ]
+	makers: [
+		{
+			name: '@electron-forge/maker-zip',
+			config: {
+				// Config here
+			}
+		},
+		{
+			name: '@electron-forge/maker-dmg',
+			config: {
+				background: './assets/dmg-background.png',
+				format: 'ULFO'
+			}
+		},
+		{
+			name: '@electron-forge/maker-pkg',
+			config: {
+				keychain: 'my-secret-ci-keychain'
+			}
+		},
+		{
+			"name": "@electron-forge/maker-squirrel",
+			"config": {
+				"certificateFile": "./cert.pfx",
+				"certificatePassword": "this-is-a-secret"
+			}
+		},
+		{
+			name: '@electron-forge/maker-deb',
+			config: {
+				options: {
+					maintainer: 'Joe Bloggs',
+					homepage: 'https://example.com'
+				}
+			}
+		},
+		{
+			name: '@electron-forge/publisher-github',
+			config: {
+				repository: {
+					owner: '14sprouj',
+					name: 'Teacher-Apocalypse'
+				},
+				prerelease: true
+			}
+		}
+	]
 }
