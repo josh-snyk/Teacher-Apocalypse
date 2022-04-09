0f6d68dab079199a5f1ce6b5c68ea2655454ba5d
Joel Sprouse <joel.sprouse11@gmail.com>
2022-04-09 15:49:40 +0100

    Zombie arms class

--- js/renderZombies.js
+++ js/renderZombies.js
@@ -14,9 +14,11 @@ $("zombie").each(function () { // Every time zombie is seen in html file, the co
 	this.appendChild(zombieArms);
 	var zombieArmL = document.createElement("div");
 	zombieArmL.classList.add("left");
+	zombieArmL.classList.add("arm");
 	zombieArms.appendChild(zombieArmL);
 	var zombieArmR = document.createElement("div");
 	zombieArmR.classList.add("right");
+	zombieArmR.classList.add("arm");
 	zombieArms.appendChild(zombieArmR);
 	var zombieLegs = document.createElement("div");
 	zombieLegs.classList.add("legs");
