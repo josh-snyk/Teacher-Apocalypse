95edb2698bab2e59ad04eaf6e8655fad8ae6a7ef
Joel Sprouse <20050599@student.stcg.ac.uk>
2022-04-27 15:44:02 +0100

    Level active

--- js/loadLevels.js
+++ js/loadLevels.js
@@ -1,9 +1,21 @@
 function loadLevel(Floor) {
     var floor = Floor;
+    $(".level-map").removeClass(" active");
     $(".level-map zombie").removeClass(" active");
-    $(".level-map.active zombie").addClass(" active");
+    
     if (Floor == 5) {
-        
         var FloorName = "FloorFive";
+        document.getElementById(FloorName).classList.add(" active")
     }
+
+    if (Floor == 4) {
+        var FloorName = "FloorFour";
+        document.getElementById(FloorName).classList.add(" active")
+    }
+
+    if (Floor == 3) {
+        var FloorName = "FloorThree";
+    }
+
+    $(".level-map.active zombie").addClass(" active");
 }
